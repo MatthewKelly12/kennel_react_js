80cacf4481332418d3dd7a5168d46b73cdb9b842
Matthew Kelly <mkonabike@gmail.com>
2018-10-06 10:54:15 -0500

    changed locations and employees to props passed to components

--- my-app/src/components/location/LocationList.js
+++ my-app/src/components/location/LocationList.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react'
 
 
-export default class EmployeeList extends Component {
+class LocationList extends Component {
     render() {
         return (
-            <article>
-				<h1>Student Kennels</h1>
-				<section>
-					<h4>Nashville North Location</h4>
-					<h5>500 Puppy Way</h5>
-				</section>
-				<section>
-					<h4>Nashville South Location</h4>
-					<h5>231 Poo Avenoo</h5>
-				</section>
-            </article>
-        );
+            <section className="locations">
+            {
+                this.props.locations.map(location =>
+                    <div key={location.id}>
+						{location.name}
+						{location.address}
+                    </div>
+                )
+            }
+            </section>
+        )
     }
 }
 
-
-
+export default LocationList
