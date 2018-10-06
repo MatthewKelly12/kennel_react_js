955600b37b492c02f40e6b24e09062d8d010d86a
Matthew Kelly <mkonabike@gmail.com>
2018-10-06 10:30:08 -0500

    set up initial components

---
+++ my-app/src/components/location/LocationList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+export default class EmployeeList extends Component {
+    render() {
+        return (
+            <article>
+				<h1>Student Kennels</h1>
+				<section>
+					<h4>Nashville North Location</h4>
+					<h5>500 Puppy Way</h5>
+				</section>
+				<section>
+					<h4>Nashville South Location</h4>
+					<h5>231 Poo Avenoo</h5>
+				</section>
+            </article>
+        );
+    }
+}
+
+
+
---
+++ my-app/src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import Kennel from "./components/Kennel"
+
+ReactDOM.render(<Kennel />, document.querySelector("#root"));
